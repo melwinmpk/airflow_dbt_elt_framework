f8df2b0eac436c19828a6d7bea3e344edb0e2f6d
melwinmpk <melwinmpk13@gmail.com>
2026-06-13 12:37:09 +0530

    updates on the project

--- dbt_projects/e_commerce/macros/metadata_update.sql
+++ dbt_projects/e_commerce/macros/metadata_update.sql
@@ -1,5 +1,5 @@
 {% macro update_metadata(tbl_name) %}
     UPDATE bronze.metadata_config
-    set last_extracxt_date = (SELECT (MAX(partition_key) || '01') as yyyymmdd FROM Bronze.{{ tbl_name }})::DATE
+    set last_extracxt_date = (SELECT (MAX(partition_key) || '01') as yyyymmdd FROM bronze.{{ tbl_name }})::DATE
     WHERE table_name = '{{ tbl_name }}'
 {% endmacro %}
